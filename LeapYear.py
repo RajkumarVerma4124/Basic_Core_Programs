b862c4e6ff06e78f209af0cc44538c82a98b88d9
RAJ <vermar4142@gmail.com>
2020-08-28 09:14:11 +0530

    Added:- The code for checking leap year or not

---
+++ LeapYear.py
@@ -0,0 +1,21 @@
+class Leapyear:
+	# instance attributes or variables	
+	def __init__(self):
+		self.year = int(input("Enter a year to check if its leap year or not : "))
+	
+	# instance methods (behaviours)
+	def checkYearLength(self):
+		if (self.year < 1000) :
+			print("Enter The Four Digit Year ")
+			exit()		
+	def checkLeapOrNot(self):
+		if (self.year % 4 == 0 and self.year % 100 != 0 or self.year % 400 == 0):
+			print("It is a leap year")
+		else:
+			print("it is not a leap year")
+
+# creating objects of class Human
+yearObj = Leapyear()
+yearObj.checkYearLength()
+yearObj.checkLeapOrNot()
+
